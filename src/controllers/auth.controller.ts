af541533e58f537e418fcf332a73ce3f7215a6c9
partharora1610 <partharora16102001@gmail.com>
2023-12-28 05:26:39 +0530

    added passwordChangedAfter method on user model

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -108,8 +108,11 @@ export const protect = async (
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    req.body.user = user;
+    if (user.changedPasswordAfter(decoded.iat)) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
+    req.body.user = user;
     next();
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
--- src/database/User.model.ts
+++ src/database/User.model.ts
@@ -4,16 +4,20 @@ import bycrptjs from "bcryptjs";
 export interface IUser extends mongoose.Document {
   email: string;
   password: string;
+  passwordChangedAt?: Date;
 
   correctPassword: (
     candidatePassword: string,
     userPassword: string
   ) => Promise<boolean>;
+
+  changedPasswordAfter: (JWTTimestamp: number) => boolean;
 }
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  passwordChangedAt: { type: Date },
 });
 
 userSchema.methods.correctPassword = async function (
@@ -22,6 +26,19 @@ userSchema.methods.correctPassword = async function (
 ) {
   console.log({ candidatePassword, userPassword });
   return await bycrptjs.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp: number) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      (this.passwordChangedAt.getTime() / 1000).toString(),
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  return false;
 };
 
 export const User = mongoose.model<IUser>("User", userSchema);
